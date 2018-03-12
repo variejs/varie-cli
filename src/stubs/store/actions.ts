8ca749fcb80fb2e36ff7abfdcddc9e56cded41db
Luke Policinski <luke.policinski@egov.com>
2018-03-12 12:07:10 -0400

    more stub

--- src/stubs/store/actions.ts
+++ src/stubs/store/actions.ts
@@ -1,6 +1,9 @@
 import { ActionContext } from "vuex";
 import RootState from "@store/rootState";
 import { tempState } from "./stateInterface";
+import HttpServiceInterface from "varie/lib/http/HttpServiceInterface";
+
+const $http : HttpServiceInterface = $app.make('$http');
 
 export const action = (
   context: ActionContext<tempState, RootState>,
