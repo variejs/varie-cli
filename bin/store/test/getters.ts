ae4621a1b076256808c938a0bcf596ff401770fc
Luke Policinski <luke@lukepolo.com>
2018-08-24 18:47:42 -0400

    removing bad files

--- bin/store/test/getters.ts
+++
@@ -1,9 +0,0 @@
-import { TestState } from "./stateInterface";
-
-export default function() {
-  return {
-    SAMPLE_GETTER: (state: TestState) => {
-      return state;
-    }
-  };
-}
--- bin/store/test/mutations.ts
+++
@@ -1,7 +0,0 @@
-import { TestState } from "./stateInterface";
-
-export default function() {
-  return {
-    SAMPLE_MUTATION: (state: TestState, data) => {}
-  };
-}
