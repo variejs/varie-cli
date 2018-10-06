c5ff294b052088cc997ed67541569c015f03e809
Luke Policinski <luke.policinski@egov.com>
2018-10-06 07:36:22 -0500

    updating route middleware template

--- src/stubs/routeMiddleware.ts
+++ src/stubs/routeMiddleware.ts
@@ -1,10 +1,10 @@
+import { Route, Location } from "vue-router";
 import { injectable, inject } from "inversify";
 import RouteMiddlewareInterface from "varie/lib/routing/RouteMiddlewareInterface";
 
 @injectable()
 export default class temp implements RouteMiddlewareInterface {
-  handler(to, from, next) {
-    // your custom logic here
-    return next();
+  handler(to: Route, from: Route, next: (route?: Location) => void) {
+    next();
   }
 }
